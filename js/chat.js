b50c13454a5d6651c9e0855a037a63bbddc3ffe6
nouramm1593 <136442131+nouramm1593@users.noreply.github.com>
2026-08-26 18:47:39 +0300

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,10 @@
+
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/6910a2f672b4a6195cfae6f9/1j9kfp72p';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
